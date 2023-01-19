931a362db424a6bec912da19592ee5ba3a57f88d
Bhattacherjee <23BhattacherjeeE@mydacc.org>
2023-01-19 12:30:05 -0500

    a

--- process.php
+++ process.php
@@ -2,7 +2,12 @@
     <body>
         <?php
             if($_SERVER["REQUEST_METHOD"] == "POST"){
-                echo "Method: Post";
+                echo "Method: Post<br><br>";
+                if(empty($_POST["Firstname"])){
+                    echo "No first name provided!";
+                }else{
+                echo "First Name: ". $_POST["firstname"];
+                }
             }else{
                 echo "Method: Get";
             }
