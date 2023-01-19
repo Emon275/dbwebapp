9f9a06fd09beed5bbc3a13faea068ffa08f8dfe6
Bhattacherjee <23BhattacherjeeE@mydacc.org>
2023-01-19 12:13:57 -0500

    value

---
+++ process.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <?php
+            if($_SERVER["REQUEST_METHOD"] == "POST"){
+                echo "Method: Post";
+            }else{
+                echo "Method: Get";
+            }
+        ?>
+    <body>
+<html>
